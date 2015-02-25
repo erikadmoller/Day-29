21270cd925ea85660e99af460d3bda8128771465
Erika Moller <erikamoller@gmail.com>
2015-02-24 18:55:57 -0600

    List of states reutrns from server sorted, and can be filtered.

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -2,33 +2,60 @@ angular.module('basic.controllers', ['basic.services'])
 .controller('BasicCtrl', function($scope, $http) {
 
 	$scope.filterBox = '';
+	// $scope.sortButton = true;
+	$scope.reverseArray = [];
 
-	var promise = $http.get('http://tiny-pizza-server.herokuapp.com/collections/fancy-table')
+
+	function getRequest() {
+
+		$http.get('http://tiny-pizza-server.herokuapp.com/collections/fancy-table')
 		.success(function(response) {
 
-			$scope.states = [];
-			for(var i=0; i<response.length; i++) {
+			$scope.states = response;
+			
+		$scope.states = _.sortBy($scope.states, function(element) {
+				return element.name + element.abbreviation;
+			})
 
-				console.log(i);
-				console.log(response[i]);
-
-					if(response[i].name && response[i].abbreviation) {
-						$scope.states.push(response[i]);
-						console.log($scope.states);
-					}
-			}
+		$scope.newArray = _.sortBy($scope.states, function(element) {
+				return element.name + element.abbreviation;
+			})
 
 		})
+	}
+	getRequest();
 
-	// $scope.Sort= Sort;
 
-	// $scope.sortUp = function() {
-	// 	console.log(Sort);
-	// }
+	$scope.titleClick = function() {
+		console.log($scope.titleClick);
 
-	// var resultArray1 = _.sortBy(states, function(element) {
-	// 		// return element.name;
-	// 		// return 'Running a test';
-	// 	})
+		$scope.reverseArray = $scope.newArray;
+		$scope.newArray = $scope.reverseArray.reverse();
+		
+		};
+
+	$scope.abbClick = function() {
+
+		$scope.states = _.sortBy($scope.states, function(element) {
+				return element.abbreviation + element.name;
+			});
+
+	};
+
+// filter expects and array and a function and if function returns true, than the element will return, and return false it gets removed
+
+	$scope.$watch('filterBox', function() {
+
+
+
+		$scope.newArray = _.filter($scope.states, function(element) {
+			console.log(element.name.toLowerCase());
+			console.log($scope.filterBox.toLowerCase());
+			return element.name.toLowerCase().indexOf($scope.filterBox.toLowerCase()) === 0;
+
+		});
+		console.log($scope.newArray);
+	});
 
 });
+
--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,8 +1,8 @@
 angular.module('basic.services', [])
 
-	.factory('Sort', function() {
-		var resultArray1 = _.sortBy(states, function(element) {
-			// return element.name;
-			return 'Running a test';
-		})
-	})
+	// .factory('Sort', function() {
+	// 	var resultArray1 = _.sortBy(states, function(element) {
+	// 		// return element.name;
+	// 		// return 'Running a test';
+	// 	})
+	// })
